f8c9d497e9c1c9b8ba254073a800b23ffe20693e
Cristiano Lopes Molin <cristianolmolin@outlook.com>
2023-03-08 11:28:26 +0000

    Stiling page

---
+++ src/app/modules/home/components/todo-input-add-item/todo-input-add-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-item',
+  templateUrl: './todo-input-add-item.component.html',
+  styleUrls: ['./todo-input-add-item.component.scss']
+})
+export class TodoInputAddItemComponent {
+
+}
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+//COMPONENTS
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItemComponent } from './components/todo-input-add-item/todo-input-add-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+//PAGES
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    TodoListComponent,
+    TodoInputAddItemComponent,
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
